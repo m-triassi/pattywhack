1262c1997a1356db9b67efc7d28488487c9a38f4
Massimo Triassi <kidmax14@hotmail.com>
2016-11-02 10:49:10 -0400

    Navbar Style Jquery
    Added Navbar Includes
    Made some basic Style Sheets
    Added Jquery

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <html>
+<link rel="stylesheet" type="text/css" href="indexStyles.css">
 
     <!-- BootStrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
     <!-- JQuery -->
-    
+<script src="jquery-3.1.1.min.js"></script>
+
+<script>
+    window.onload = function () {
+        $("body").fadeIn(950);
+    };
+</script>
     
 <head>
     <title>Patty Whack</title>
 </head>
 
-<body>
-    <h1>Welcome to Patty Whack.com<br /><small>Add some variety to your life</small></h1>
-    <input type="button" value="Login" class="btn btn-success btn-sm" style="float: right;">
+<body style="display:none;">
+    <h1>Welcome to Patty Whack.com<br />
+        <small>Add some variety to your life</small>
+        <input type="button" value="Login" class="btn btn-success btn-sm" style="float: right;">
+    </h1>
+    <?php include("includes/navbar.html"); ?>
+    
+    
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus sollicitudin est, ut laoreet enim consequat eget. In at lectus a ex maximus interdum eu id purus. In posuere vel libero in imperdiet. Praesent maximus feugiat rhoncus. Aliquam sed nulla facilisis, elementum felis vel, dictum purus. Donec lacinia nisi a massa scelerisque, in convallis magna maximus. Nulla placerat neque non aliquam venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum est eget elit tincidunt, ut accumsan ligula faucibus.
+
+    Aenean faucibus viverra placerat. Etiam ut est orci. Fusce ac posuere nunc. Etiam eu efficitur ante. Cras facilisis metus non varius faucibus. In fringilla pretium enim, nec pretium turpis ultricies a. Sed tincidunt ornare eros id tempor. Vivamus ullamcorper purus at augue bibendum, at rutrum tellus consequat.
+    </p>
     </body>
 
 </html>
