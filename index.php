464f8d704b004f8b47447c5631ef019aecdbb0d2
Massimo Triassi <kidmax14@hotmail.com>
2016-11-02 09:00:50 -0400

    Skeleton changes
    Some basic garbage added like a login button

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- JQuery -->
     
     
 <head>
@@ -11,9 +12,8 @@
 </head>
 
 <body>
-    <h1>Welcome to Patty Whack.com</h1> 
-    <h2>This is a website</h2>
-    
+    <h1>Welcome to Patty Whack.com<br /><small>Add some variety to your life</small></h1>
+    <input type="button" value="Login" class="btn btn-success btn-sm" style="float: right;">
     </body>
 
 </html>
