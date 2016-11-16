7e9affb8bd4044189e4510acd4133f951caf34db
Massimo Triassi <kidmax14@hotmail.com>
2016-11-16 09:52:48 -0500

    Careful of this Commit DATABASE.PHP
    This might over write your Database.php password, keep this in mind

--- mvc/app/database.php
+++ mvc/app/database.php
@@ -7,7 +7,7 @@ $capsule->addConnection([
 	'driver' => 'mysql',
 	'host' => 'localhost',
 	'username' => 'RandomUser',
-	'password' => 'GAtvN2yKA7wFGxyW',
+	'password' => 'm93ax7xnPjM9fEP8',
 	'database' => 'pattydb',
 	'charset' => 'utf8',
 	'collation' => 'utf8_unicode_ci',
