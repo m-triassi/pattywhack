14cfff2c1ae15975f501e4199075509472e74fa9
Massimo Triassi <kidmax14@hotmail.com>
2016-11-20 16:44:02 -0500

    Fixed Response Glitch

--- mvc/app/models/response.php
+++ mvc/app/models/response.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Response extends Eloquent
 {
-	public $timestamp = false;
+	public $timestamps = false;
 	protected $table = 'response';
 	protected $primaryKey = 'response_id';	
 	protected $fillable = ['question_id', 'username', 'answer'];
--- mvc/app/models/status.php
+++ mvc/app/models/status.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Status extends Eloquent
 {
-	public $timestamp = false;
+	public $timestamps = false;
 	protected $table = 'status';
 	protected $primaryKey = 'status_id';	
 	protected $fillable = ['status_name'];
